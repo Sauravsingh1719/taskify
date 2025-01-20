7e145900ff1b1ed8e7e4b5f6fdcdce32784909a5
Saurav Singh <Sauravs1719@gmail.com>
2025-01-20 15:50:52 +0530

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Footer = () => {
+    const getCurrentYear = () => new Date().getFullYear();
+  return (
+    <div className="bg-black text-white px-4 sm:px-10 md:px-20 lg:px-52 py-5">
+        <div className='flex flex-row justify-between'>
+        <div>
+            <ul className='flex flex-col gap-2'>
+                <li>
+                    Home
+                </li>
+                <li>
+                    Login
+                </li>
+                <li>
+                    Signup
+                </li>
+            </ul>
+        </div>
+        <div>
+            <a href='https://portfolio160.vercel.app/'>
+                Contact me
+            </a>
+        </div>
+        </div>
+        <div className='flex flex-col gap-2 text-center justify-center items-center'>
+            <h1>Made with ❤️ in 🇮🇳 </h1>
+            <h1> © {getCurrentYear()} All rights reserved. </h1>
+        </div>
+        
+            
+    </div>
+  )
+}
+
+export default Footer
