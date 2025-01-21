6a6a7d6e41e19173e12b14c8860cb44f0ac343b2
Saurav Singh <Sauravs1719@gmail.com>
2025-01-22 01:50:21 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,54 +1,57 @@
-'use client'
+'use client';
 
 import React from 'react';
 import Link from 'next/link';
 import { useSession, signOut } from 'next-auth/react';
 import { Button } from '@/components/ui/button';
 import { User } from 'next-auth';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
 function Navbar() {
   const { data: session } = useSession();
-  const user : User = session?.user;
+  const user: User = session?.user;
   const router = useRouter();
+  const pathname = usePathname(); // Get the current path
 
+  const isOnDiaryPage = pathname === '/diary';
 
   return (
-    <div >
-    <nav className="p-4 md:p-6 shadow-md bg-black text-white">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <a href="/" className="text-xl font-bold mb-4 md:mb-0">
-          Taskify
-        </a>
-        {session ? (
-          <>
-          <div className='flex flex-row gap-5'>
-            <Button onClick={() => signOut()} className="w-full md:w-auto bg-slate-100 text-black" variant='outline'>
-              Logout
-            </Button>
-              <a
-                href="/diary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  router.push('/diary'); 
-                }}
+    <div>
+      <nav className="p-4 md:p-6 shadow-md bg-black text-white">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+          <a href="/" className="text-xl font-bold mb-4 md:mb-0">
+            Taskify
+          </a>
+          {session ? (
+            <div className="flex flex-row gap-5">
+              <Button
+                onClick={() => signOut()}
+                className="w-full md:w-auto bg-slate-100 text-black"
+                variant="outline"
               >
-                <Button className="w-full md:w-auto bg-slate-100 text-black" variant='outline'>
-                Diary
-                </Button>
-              </a>
-
+                Logout
+              </Button>
+              <Button
+                onClick={() => router.push(isOnDiaryPage ? '/' : '/diary')}
+                className="w-full md:w-auto bg-slate-100 text-black"
+                variant="outline"
+              >
+                {isOnDiaryPage ? 'Home' : 'Diary'}
+              </Button>
             </div>
-          </>
-        ) : (
-          <Link href="/sign-in">
-            <Button className="w-full md:w-auto bg-slate-100 text-black" variant={'outline'}>Login</Button>
-          </Link>
-        )}
-      </div>
-    </nav>
+          ) : (
+            <Link href="/sign-in">
+              <Button
+                className="w-full md:w-auto bg-slate-100 text-black"
+                variant="outline"
+              >
+                Login
+              </Button>
+            </Link>
+          )}
+        </div>
+      </nav>
     </div>
-   
   );
 }
 
